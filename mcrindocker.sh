18db7430e0c49e8681bffbf26de5bf424568b0a4
Xizhen Tan (Wicresoft North America) <v-xiztan@microsoft.com>
2018-07-16 02:42:22 -0700

    update

--- mcrindocker.sh
+++ mcrindocker.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+chmod 777 ./etc/hosts
 echo "0.0.0.0 mcr.microsoft.com" >> /etc/hosts
 
 docker info
@@ -8,5 +9,6 @@ docker run --rm --link mcr-in-docker:docker docker sh
 docker run --rm --link mcr-in-docker:docker docker info
 cat /etc/hosts
 docker run --rm --link mcr-in-docker:docker docker pull mcr.microsoft.com/linux-samples:latest
+docker run --rm --link mcr-in-docker:docker docker images
 docker run --rm --link mcr-in-docker:docker docker images mcr.microsoft.com/linux-samples:latest
 docker run --rm --link mcr-in-docker:docker docker rmi -f mcr.microsoft.com/linux-samples:latest
