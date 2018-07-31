193b90899f275ad8be3b6e56d322b039fc561d5f
callentan <39490917+callentan@users.noreply.github.com>
2018-07-31 13:46:37 +0800

    Update shelltest.sh

--- shelltest.sh
+++ shelltest.sh
@@ -1,11 +1,5 @@
-arr=("123_df" "asdasdsaudsus" "12312")
-
-for a in ${arr[@]}
-do
-if [[$a =~ "_df"] || [$a =~ "us"]]
-then
-echo dfus
-else
-echo null
-fi
-done
+git clone -n https://v-xiztan.visualstudio.com/azurecr-test/_git/iis-docker-vsts
+expect "Username"
+send "v-xiztan\n"
+expect "Password"
+send "xwquuppck6ahf4zuppk5zz5kiw3j2inevokkb2ouws2ux7be5tmqgit\n"
