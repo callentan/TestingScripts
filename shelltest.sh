7d12db9526e4b52e9c6456e3f0c2e4e1f64c8739
callentan <39490917+callentan@users.noreply.github.com>
2018-07-31 13:57:53 +0800

    Update shelltest.sh

--- shelltest.sh
+++ shelltest.sh
@@ -7,6 +7,6 @@
 #send "xwquuppck6ahf4zuppk5zz5kiw3j2inevokkb2ouws2ux7be5tmqgit\n"
 
 git clone https://v-xiztan.visualstudio.com/azurecr-test/_git/iis-docker-vsts <<EOF
-v-xiztan\n
-xwquuppck6ahf4zuppk5zz5kiw3j2inevokkb2ouws2ux7be5tmqgit
+"v-xiztan\n"
+"xwquuppck6ahf4zuppk5zz5kiw3j2inevokkb2ouws2ux7be5tmqgit"
 EOF
