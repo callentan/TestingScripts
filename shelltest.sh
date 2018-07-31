c4d3faff0dfb02029731dc73a72975c848ca0812
callentan <39490917+callentan@users.noreply.github.com>
2018-07-31 13:54:51 +0800

    Update shelltest.sh

--- shelltest.sh
+++ shelltest.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
-git clone -n https://v-xiztan.visualstudio.com/azurecr-test/_git/iis-docker-vsts
-expect "Username"
-send "v-xiztan\n"
-expect "Password"
-send "xwquuppck6ahf4zuppk5zz5kiw3j2inevokkb2ouws2ux7be5tmqgit\n"
+#git clone -n https://v-xiztan.visualstudio.com/azurecr-test/_git/iis-docker-vsts
+#expect "Username"
+#send "v-xiztan\n"
+#expect "Password"
+#send "xwquuppck6ahf4zuppk5zz5kiw3j2inevokkb2ouws2ux7be5tmqgit\n"
+
+git clone -n https://v-xiztan.visualstudio.com/azurecr-test/_git/iis-docker-vsts <<EOF
+v-xiztan\n
+xwquuppck6ahf4zuppk5zz5kiw3j2inevokkb2ouws2ux7be5tmqgit
+EOF
